0ddc4b3443511722d4d6790fb0be82b4ad2236dc
stanislav <semioys@gmail.com>
2020-08-23 21:39:01 +0300

    Validation was added

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,5 +1,19 @@
+import { IsString } from "class-validator";
+
 export class CreateCoffeeDto {
+    @IsString({
+        message: "Please enter a correct name of coffee",
+    })
     readonly name: string;
+
+    @IsString({
+        message: "Please enter a correct brand of coffee",
+    })
     readonly brand: string;
+
+    @IsString({
+        each: true,
+        message: "Please enter a correct flawors",
+    })
     readonly flawors: string[];
 }
